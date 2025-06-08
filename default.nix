2a31474bf378ab5313daea786bdba2b59ab9d7eb
randomizedcoder dave.seddon.ca@gmail.com <dave.seddon.ca@gmail.com>
2025-06-08 09:21:56 -0700

    BUILD.bazel back in the sysroot

--- default.nix
+++ default.nix
@@ -150,10 +150,9 @@ let
       ln -sf llvm-c++filt c++filt
       ln -sf llvm-cov gcov
 
-    # Not embedding the bazel config in the sysroot.  This means the sysroot only has binaries, headers, and libraries.
-    #   # Copy Bazel configuration files
-    #   cp ${./bazel/BUILD.bazel} $out/sysroot/BUILD.bazel
-    #   cp ${./bazel/cc_toolchain_config.bzl} $out/sysroot/cc_toolchain_config.bzl
+    # Copy Bazel configuration file
+    cp ${./bazel/BUILD.bazel} $out/sysroot/BUILD.bazel
+    # cp ${./bazel/cc_toolchain_config.bzl} $out/sysroot/cc_toolchain_config.bzl
     '';
 
     meta = with pkgs.lib; {
